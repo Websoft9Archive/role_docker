2d7b2743f36f69ad2f6b9d29094f42b3ccdefc3e
zengxc <63622990+zxc916684603@users.noreply.github.com>
2020-11-18 16:44:58 +0800

    Update check_dockerapp_version.sh

--- templates/check_dockerapp_version.sh
+++ templates/check_dockerapp_version.sh
@@ -4,7 +4,7 @@ docker_compose_file_url=/data/wwwroot/{{docker_appname}}/docker-compose.yml
 check_dockerapp_version_cmd=$(cat $docker_compose_file_url |grep APP_COMMON_DOCKER_VERSION |cut -d ":" -f 2)
 
 # check dockerapp version
-sudo echo "{{docker_appname}}_version:" $(check_dockerapp_version_cmd) |sudo tee -a /data/logs/install_version.txt
+sudo echo "{{docker_appname}}_version: $(check_dockerapp_version_cmd)" |sudo tee -a /data/logs/install_version.txt
 
 # delete check version line
 sed -i '/APP_COMMON_DOCKER_VERSION/d' $docker_compose_file_url
